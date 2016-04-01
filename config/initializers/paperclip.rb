64b97afc26c8f51050e9b24332dcee609bcf1f77
Joshua Munene <kenyadevelop@gmail.com>
2016-04-01 10:10:38 -0700

    heroku amazon config. Add paperclip.rb initializer.

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,5 @@
+Paperclip::Attachment.default_options[:url] = ':s3-us-west-1.amazonaws.com'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+# Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+
+# https://s3-us-west-1.amazonaws.com/movie-review-dev/uploads/movie/image/13/poster_durieux-back-to-the-future2.jpg
