b5988eed1d22776855eced03fff8c29a246c55cd
Joshua Munene <kenyadevelop@gmail.com>
2016-03-31 19:24:21 -0700

    Add comments to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+	has_many :comments
 	belongs_to :user
 	has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
